6644064fd36864e414f21a1cdf7fb961971deb95
AmbroB <ambroziubaban@gmail.com>
2017-05-16 19:24:43 +0300

    Logout done

--- MovieScheduler/app/src/main/java/com/moviescheduler/fragment/FirstFragment.java
+++ MovieScheduler/app/src/main/java/com/moviescheduler/fragment/FirstFragment.java
@@ -70,7 +70,7 @@ public class FirstFragment extends Fragment {
 
         if (emailText.getText().length()!=0&& emailText.getText().length()!=0) {
 
-            String url = "https://back-end-school-project.herokuapp.com/login";
+            String url = "https://back-end-school-project.herokuapp.com/api/login";
 
             AsyncHttpClient client = new AsyncHttpClient();
 
@@ -94,8 +94,8 @@ public class FirstFragment extends Fragment {
                             userDetailsJson=h.getValue();
                     }
                     if(authToken!=null && userDetailsJson!=null){
-                        tokenHandler.saveToken(authToken,myView.getContext());
-                        tokenHandler.saveDetails(userDetailsJson,myView.getContext());
+                        Token.saveToken(authToken,myView.getContext());
+                        Token.saveDetails(userDetailsJson,myView.getContext());
 
                         Toast.makeText(myView.getContext(), "Login success!", Toast.LENGTH_LONG).show();
 
