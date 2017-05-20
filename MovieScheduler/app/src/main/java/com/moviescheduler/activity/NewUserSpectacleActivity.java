a85f56a0db6a2fccfcb6a51414dba282404a565d
AmbroB <ambroziubaban@gmail.com>
2017-05-20 04:43:23 +0300

    Activity for booking- 3rd try

--- MovieScheduler/app/src/main/java/com/moviescheduler/activity/NewUserSpectacleActivity.java
+++ MovieScheduler/app/src/main/java/com/moviescheduler/activity/NewUserSpectacleActivity.java
@@ -4,8 +4,12 @@ package com.moviescheduler.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -33,8 +37,10 @@ public class NewUserSpectacleActivity extends AppCompatActivity {
     Long idSpectacle;
     String spectacleDescription;
     Spinner dateSpinner;
-
+    Spinner seatsSpinner;
+    Button bookButton;
     ArrayAdapter<String> listAdapter;
+    ArrayAdapter<Integer> arrayAdapter;
     private static final String TOKEN_NAME = "Authorization";
     private static final String TOKEN_VALUE = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyenZzOTVAZ21haWwuY29tIiwiZXhwIjoxNDk2MDUxOTE2fQ.HZ4eVINa6Ng4cQPTp2HgttpRKnaZ2D9n4tndAkXd-VSZGvzr2BNiboM1FNMBtW8zc-Gw0InminMT9D1TmABLlw";
 
@@ -52,11 +58,26 @@ public class NewUserSpectacleActivity extends AppCompatActivity {
          */
         idSpectacle=getIntent().getLongExtra("SpectacleId",0);
        spectacleDescription=getIntent().getStringExtra("SpectacleDescription");
-
+       initButton();
+        initDesctiption(spectacleDescription);
         onGetSpectacleSchedule(idSpectacle);
 
     }
-    void initSpinner(List<SpectacleSchedule> spectacleSchedules){
+
+    void initDesctiption(String descriptionText){
+        TextView descriptionTextView= (TextView) findViewById(R.id.descriptionText);
+        descriptionTextView.setText(descriptionText);
+    }
+    void initButton(){
+        bookButton=(Button) findViewById(R.id.bookNowButton);
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    void initDateSpinner(final List<SpectacleSchedule> spectacleSchedules){
         List<String> list=new ArrayList<String>();
         if(spectacleSchedules.isEmpty()){
             list.add("-No show schduled-");
@@ -74,12 +95,39 @@ public class NewUserSpectacleActivity extends AppCompatActivity {
         listAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dateSpinner.setAdapter(listAdapter);
 
+        dateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onGetSpectacleSeats(spectacleSchedules.get(position).getIdSpectacleSchedule());
+                setPrice(spectacleSchedules.get(position).getPrice());
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //TODO:DateSpinner onItemSelected -> start the fragment
 
 
     }
+    void initSetsSpinner(Integer[] allEmptySeats){
+        if(allEmptySeats.length==0){
+            allEmptySeats=new Integer[1];
+            allEmptySeats[0]=-1;
+            bookButton.setClickable(false);
+        }
+        else{
+            bookButton.setClickable(true);
+        }
+
+        seatsSpinner=(Spinner) findViewById(R.id.seatSpinner);
+        arrayAdapter=new ArrayAdapter<Integer>(this,android.R.layout.simple_selectable_list_item,allEmptySeats);
+       arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seatsSpinner.setAdapter(arrayAdapter);
+    }
+
     String getDateAsString(SpectacleSchedule spectacleSchedule){
         Long l=Long.parseLong(spectacleSchedule.getSpectacleDate());
         Date date=new Date(l);
@@ -104,7 +152,7 @@ public class NewUserSpectacleActivity extends AppCompatActivity {
 
                   */
                     List<SpectacleSchedule> spectaclesReceives = new Gson().fromJson(response, listType);
-                    initSpinner(spectaclesReceives);
+                    initDateSpinner(spectaclesReceives);
 
 
             }
@@ -125,4 +173,47 @@ public class NewUserSpectacleActivity extends AppCompatActivity {
         };
         que.add(stringRequest);
     }
+    void onGetSpectacleSeats(long id){
+        RequestQueue que = Volley.newRequestQueue(getApplicationContext());
+        String url = url="https://back-end-school-project.herokuapp.com/api/get-empty-seats/idSchedule=";
+        url=url+id;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Type listType = new TypeToken<Integer[]>() {
+                }.getType();
+                /**
+                 @NOTE 1 - i get an NullPointerException at listType
+                 @NOTE 2 - Problem with json parsing Date SOLVED!
+
+                 */
+                Integer[] allEmptySeats = new Gson().fromJson(response, listType);
+                initSetsSpinner(allEmptySeats);
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast toast = Toast.makeText(getApplicationContext(),error.getStackTrace()+"", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headerParams = new HashMap<String, String>();
+                // headerParams.put(TOKEN_NAME, Token.getToken(getContext()));
+                headerParams.put(TOKEN_NAME, TOKEN_VALUE);
+                return headerParams;
+            }
+        };
+        que.add(stringRequest);
+    }
+    void setPrice(int price){
+        TextView priceValue=(TextView) findViewById(R.id.priceValue);
+        priceValue.setText(price+"");
+    }
+
 }
